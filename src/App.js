e0dafacb792dd62ca39f4c4d4d9b025606ae8e3b
Nabin Dhakal <nabindhakal33@gmail.com>
2022-07-27 18:09:03 +0545

    initial setup #2

--- src/App.js
+++ src/App.js
@@ -7,17 +7,7 @@ function App() {
   return (
 
     <div className="App">
-      <AccessAlarmIcon />
-      <Card style={{ width: "18rem" }}>
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
+      Have fun coding!
     </div>
   );
 }
